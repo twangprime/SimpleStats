4c0e923b60b199f92ecfb65d3f9169a9d1e0c905
tiyuwang <tiyuwang@tiyuwangdeMacBook-Pro.local>
2022-06-12 22:21:21 -0400

    working site

---
+++ buildchart.js
@@ -0,0 +1,35 @@
+function BuildChart(labels, values, chartTitle) {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new chartTitle(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: chartTitle, 
+                data: values,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [ // Add custom color borders
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1 
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false, 
+        }
+    });
+    return myChart;
+}
